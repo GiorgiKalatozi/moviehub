0f3e717ea1063dfcba457761921e19989f2859bb
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-08-25 14:06:41 -0800

    feat: favorite model

---
+++ server/src/models/favorite.model.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+
+const favoriteSchema = new mongoose.Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    mediaType: {
+      type: String,
+      enum: ["tv", "movie"],
+      required: true,
+    },
+    mediaId: {
+      type: String,
+      required: true,
+    },
+    mediaTitle: {
+      type: String,
+      required: true,
+    },
+    mediaPoster: {
+      type: String,
+      required: true,
+    },
+    mediaRate: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const favoriteModel = mongoose.model("Favorite", favoriteSchema);
+
+export default favoriteModel;
