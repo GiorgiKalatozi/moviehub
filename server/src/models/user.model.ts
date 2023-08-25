9d20885e950d50c2fb507fb13559976691926828
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-08-25 13:49:01 -0800

    feat: user model

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -23,3 +23,23 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.methods.setPassword = function (password: string) {
+  this.salt = crypto.randomBytes(16).toString("hex");
+
+  this.password = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, "sha512")
+    .toString("hex");
+};
+
+userSchema.methods.validPassword = function (password: string) {
+  const hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, "sha512")
+    .toString("hex");
+
+  return this.password === hash;
+};
+
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;
