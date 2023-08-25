7667b88642d8570d66aaf09ab4c06ec39b5b0d99
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-08-25 13:59:14 -0800

    feat: review schema

---
+++ server/src/models/review.model.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    mediaType: {
+      type: String,
+      enum: ["tv", "movie"],
+      required: true,
+    },
+    mediaId: {
+      type: String,
+      required: true,
+    },
+    mediaTitle: {
+      type: String,
+      required: true,
+    },
+    mediaPoster: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
