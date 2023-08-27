f32f245b1facde7e49244ec869bad98b888b67ff
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-08-27 14:16:33 -0800

    feat: person controller

---
+++ server/src/controllers/person.controller.ts
@@ -0,0 +1,36 @@
+import { responseHandler } from "@/handlers";
+import { tmdbApi } from "@/tmdb";
+import { RequestHandler } from "express";
+
+const personDetails: RequestHandler<{ personId: string }> = async (
+  req,
+  res,
+  next
+) => {
+  try {
+    const { personId } = req.params;
+    const person = await tmdbApi.personDetail({ personId });
+
+    return responseHandler.ok(res, person);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const personMedias: RequestHandler<{ personId: string }> = async (
+  req,
+  res,
+  next
+) => {
+  try {
+    const { personId } = req.params;
+
+    const medias = await tmdbApi.personMedias({ personId });
+
+    return responseHandler.ok(res, medias);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default { personDetails, personMedias };
