b8d61c72909e182287a53398779ba0010b42dfb0
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-08-28 10:30:51 -0800

    feat: add favorites route

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -12,7 +12,7 @@ router.post(
   body("username")
     .exists()
     .withMessage("username is required")
-    .isLength({ min: 0 })
+    .isLength({ min: 8 })
     .withMessage("username minimum 8 characters")
     .custom(async (value) => {
       const user = await UserModel.findOne({ username: value });
@@ -21,12 +21,12 @@ router.post(
   body("password")
     .exists()
     .withMessage("password is required")
-    .isLength({ min: 0 })
+    .isLength({ min: 8 })
     .withMessage("password minimum 8 characters"),
   body("confirmPassword")
     .exists()
     .withMessage("confirmPassword is required")
-    .isLength({ min: 0 })
+    .isLength({ min: 8 })
     .withMessage("confirmPassword minimum 8 characters")
     .custom((value, { req }) => {
       if (value !== req.body.password)
@@ -52,7 +52,7 @@ router.post(
   body("password")
     .exists()
     .withMessage("password is required")
-    .isLength({ min: 0 })
+    .isLength({ min: 8 })
     .withMessage("password minimum 8 characters"),
   requestHandler.validate,
   userController.signIn
@@ -64,17 +64,17 @@ router.put(
   body("password")
     .exists()
     .withMessage("password is required")
-    .isLength({ min: 0 })
+    .isLength({ min: 8 })
     .withMessage("password minimum 8 characters"),
   body("newPassword")
     .exists()
     .withMessage("newPassword is required")
-    .isLength({ min: 0 })
+    .isLength({ min: 8 })
     .withMessage("newPassword minimum 8 characters"),
   body("confirmNewPassword")
     .exists()
     .withMessage("confirmNewPassword is required")
-    .isLength({ min: 0 })
+    .isLength({ min: 8 })
     .withMessage("confirmNewPassword minimum 8 characters")
     .custom((value, { req }) => {
       if (value !== req.body.newPassword)
@@ -83,4 +83,31 @@ router.put(
     }),
   requestHandler.validate,
   userController.updatePassword as any
+);
+
+router.get("/info", tokenMiddleware.auth, userController.getInfo as any);
+
+router.get(
+  "/favorites",
+  tokenMiddleware.auth,
+  favoriteController.getFavoritesOfUser as any
+);
+
+router.post(
+  "/favorites",
+  tokenMiddleware.auth,
+  body("mediatype")
+    .exists()
+    .withMessage("mediatype is required")
+    .custom((type) => ["movie", "tv"].includes(type))
+    .withMessage("mediatype is invalid"),
+  body("mediaId")
+    .exists()
+    .withMessage("mediaId is required")
+    .isLength({ min: 1 })
+    .withMessage("mediaId can not be empty"),
+  body("mediaTitle").exists().withMessage("mediaTitle is required"),
+  body("mediaPoster").exists().withMessage("mediaPoster is required"),
+  body("mediaRate").exists().withMessage("mediaRate is required"),
+  favoriteController.addFavorite as any
 );
