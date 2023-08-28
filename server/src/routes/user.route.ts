d3b5cd170a549513ff1c5c6425df94786aea5891
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-08-28 09:08:58 -0800

    feat: signup route

---
+++ server/src/routes/user.route.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { body } from "express-validator";
+import { favoriteController, userController } from "@/controllers";
+import { requestHandler, responseHandler } from "@/handlers";
+import { UserModel } from "@/models";
+import { tokenMiddleware } from "@/middlewares";
+
+const router = express.Router();
+
+router.post(
+  "/signup",
+  body("username")
+    .isLength({ min: 0 })
+    .withMessage("username minimum 8 characters")
+    .custom(async (value) => {
+      const user = await UserModel.findOne({ username: value });
+      if (user) return Promise.reject("username already used");
+    }),
+  body("password")
+    .isLength({ min: 0 })
+    .withMessage("password minimum 8 characters"),
+  body("confirmPassword")
+    .isLength({ min: 0 })
+    .withMessage("confirm password minimum 8 characters")
+    .custom((value, { req }) => {
+      if (value !== req.body.password)
+        throw new Error("confirmPassword not match");
+      return true;
+    }),
+  body("email").isEmail().withMessage("enter a valid email"),
+  requestHandler.validate,
+  userController.signUp
+);
